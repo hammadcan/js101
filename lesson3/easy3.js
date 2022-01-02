ca3ed41a5ad3de23d8d8197b2c3143f163230427
Hammad Khan <hammadkhan91@live.com>
2022-01-02 12:29:01 -0800

    Easy 3

---
+++ lesson3/easy3.js
@@ -0,0 +1,31 @@
+/*Write three different ways to remove all
+of the elements from the following array:
+ */
+// let numbers = [1, 2, 3, 4];
+// numbers.length = 0;
+// console.log(numbers);
+
+// What will the following code output?
+// console.log([1, 2, 3] + [4, 5]);
+
+// let str1 = "hello there";
+// let str2 = str1;
+// str2 = "goodbye!";
+// console.log(str1);
+//Pass by value
+
+// let arr1 = [{ first: "value1" }, { second: "value2" }, 3, 4, 5];
+// let arr2 = arr1.slice();
+// arr2[0].first = 42;
+// console.log(arr1);
+//Pass by reference
+
+// function isColorValid(color) {
+//   Boolean(color === "blue");
+//   return Boolean(color === "green");
+// }
+
+// function isColorValid(color) {
+//   return color === "blue" || color === "green";
+// }
+// console.log(isColorValid("red"));
