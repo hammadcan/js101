1e8f1d5c97c325a84bc79538a860ece976759d60
Hammad Khan <hammadkhan91@live.com>
2022-01-02 16:44:09 -0800

    Hard 1 Problems

--- lesson3/hard1.js
+++ lesson3/hard1.js
@@ -1,16 +1,18 @@
-function first() {
-  return {
-    prop1: "hi there"
-  };
-}
+// function first() {
+//   return {
+//     prop1: "hi there"
+//   };
+// }
+//
+// function second() {
+//   return
+//   {
+//     prop1: "hi there"
+//   };
+// }
+//
+// console.log(first());
+// console.log(second());
 
-function second() {
-  return
-  {
-    prop1: "hi there"
-  };
-}
-
-console.log(first());
-console.log(second());
+//Nothing will be logged in the 2nd function
 
